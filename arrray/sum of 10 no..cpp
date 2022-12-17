e0e20a739d43c3661d91ebcc6bc5f5616d6493c3
soumyaditya bauri <116110445+Soumyadityaa@users.noreply.github.com>
2022-12-18 03:02:11 +0530

    Update sum of 10 no..cpp

--- arrray/sum of 10 no..cpp
+++ arrray/sum of 10 no..cpp
@@ -3,15 +3,15 @@
 int main()
 {
 	
-	int a[10],i,s;
-	s=0;
+	int a[10],i,sum;
+	sum=0;
 	for(i=0;i<=9;i++)
 	{
 		printf("enter the no. : ");
 		scanf("%d",&a[i]);
-		s=s+a[i];
+		sum=sum+a[i];
 		
 	}
-	printf("answer = %d ",s);
+	printf("answer = %d ",sum);
 	getch();
 }
